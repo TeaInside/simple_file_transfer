4862f248968c2043213911778b1ed5926c5a564d
Ammar Faizi <ammarfaizi2@gmail.com>
2020-05-10 14:01:20 +0700

    Clean up

--- ammarfaizi2/data_structure.h
+++ ammarfaizi2/data_structure.h
@@ -2,11 +2,12 @@
 #ifndef DATA_STRUCTURE_H
 #define DATA_STRUCTURE_H
 
-#include <stdlib.h>
+#include <stdint.h>
+#define BUFFER_SIZE 1500
 
-typedef struct {
-  size_t len; // the length of data to be sent.
-  char *data; // data to be sent
+typedef struct __attribute__((__packed__)) {
+  uint64_t len; // the length of data to be sent.
+  char data[1]; // must be "struct hack".
 } packet;
 
 #endif
