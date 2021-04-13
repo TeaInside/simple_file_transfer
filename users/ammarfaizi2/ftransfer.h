13bd4bd6a02a4b842251e49d2e08309f4ae2af66
Ammar Faizi <ammarfaizi2@gmail.com>
2021-04-14 06:36:41 +0700

    Initial commit
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

---
+++ users/ammarfaizi2/ftransfer.h
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Simple file transfer header
+ *
+ * Copyright (C) 2021  Your Name <your_email@domain.com>
+ */
+
+#ifndef FTRANSFER_H
+#define FTRANSFER_H
+
+#include <stdint.h>
+
+int run_server(int argc, char *argv[]);
+int run_client(int argc, char *argv[]);
+
+typedef struct packet_t {
+	uint8_t		filename_len;
+	char		filename[0xffu];
+	uint64_t	file_size;
+} packet_t;
+
+#endif
---
+++ users/ammarfaizi2/server.c
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Simple file transfer server
+ *
+ * Copyright (C) 2021  Your Name <your_email@domain.com>
+ */
+
+#include "ftransfer.h"
+
+
+int run_server(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	return 0;
+}
